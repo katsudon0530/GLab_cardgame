fc192375099e97df773b1969d85c73da3215b2f2
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-02-17 23:53:37 +0900

    [Add]入ってきたタイミングで色が変わるように　

--- Assets/Scripts/UI/ButtonUI.cs
+++ Assets/Scripts/UI/ButtonUI.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ButtonUI : MonoBehaviour
@@ -9,24 +10,45 @@ public class ButtonUI : MonoBehaviour
     /// <summary>
     ///　クリックされたときに発火するAction
     /// </summary>
-    public Action OnClick;
+    private Action _onClick;
+    public Action OnClick{get => _onClick; set => _onClick = value;}
+    
+    [FormerlySerializedAs("_color")] [SerializeField]
+    Color _OnCursorColor;
 
     private void Awake()
     {
         _image = GetComponent<Image>();
+        _onClick += ForDebug;
+    }
+    void ForDebug()
+    {
+        Debug.Log("click");
     }
 
     private void Update()
     {
-       Debug.Log(Clicked()); 
+        if (OnMouseCursor())
+        {
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                _onClick?.Invoke();　//　Actionを発火（Actionの中身がnullの場合を考慮）
+            }
+            _image.color = Color.Lerp(_image.color, _OnCursorColor, Time.deltaTime);
+            
+        }
+       
     }
 
-    private bool Clicked()
+    private bool OnMouseCursor()
     {
         RectTransform rectTransform = GetComponent<RectTransform>();
         Vector2 localPoint = Vector2.zero;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
-            rectTransform, Input.mousePosition, Camera.main, out localPoint);
-        return rectTransform.rect.Contains(localPoint);
-    } 
+            rectTransform, Input.mousePosition, Camera.main, out localPoint); // 第一引数に入れたRectTransformに対する第二引数のローカル座標をRectTransform基準で返す
+        return rectTransform.rect.Contains(localPoint);　//　カーソルのローカル座標が範囲内にあるかどうかを返す
+    }
+
+  
 }
