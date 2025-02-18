edc7353e4f83c991fd9c6e315d05a67d3b980fd5
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-02-18 21:22:47 +0900

    [Add]色が切り替わる機能の追加　遷移がfadeしない問題は未解決

--- Assets/Scripts/UI/ButtonUI.cs
+++ Assets/Scripts/UI/ButtonUI.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
@@ -11,14 +13,46 @@ public class ButtonUI : MonoBehaviour
     ///　クリックされたときに発火するAction
     /// </summary>
     private Action _onClick;
+    
+    /// <summary>
+    ///　このActionにクリック時に発火させたい処理を追加する
+    /// </summary>
     public Action OnClick{get => _onClick; set => _onClick = value;}
     
-    [FormerlySerializedAs("_color")] [SerializeField]
-    Color _OnCursorColor;
+    [FormerlySerializedAs("_OnCursorColor")] [SerializeField]
+    Color _onCursorColor;
+    
+    [FormerlySerializedAs("_OnCursorColor")] [SerializeField]
+    Color _defaultColor;
+    
+    private bool _onCursor = false;
+    
+    private Coroutine _colorCoroutine;
+    
+    [SerializeField]
+    private float _colorChangeTime = 0.5f;  
+
+    public bool OnCursor
+    {
+        get => _onCursor;
+        set
+        {
+            if (_onCursor != value)
+            {
+                _onCursor = value;
+                if (_colorCoroutine != null)
+                {
+                    StopCoroutine(_colorCoroutine);
+                }
+                _colorCoroutine = StartCoroutine(ChangeColor(_onCursor ? _onCursorColor : _defaultColor));
+            }
+        }
+    }
 
     private void Awake()
     {
         _image = GetComponent<Image>();
+        _image.color = _defaultColor;
         _onClick += ForDebug;
     }
     void ForDebug()
@@ -28,26 +62,48 @@ public class ButtonUI : MonoBehaviour
 
     private void Update()
     {
-        if (OnMouseCursor())
+        OnMouseCursor();
+        if (OnCursor)
         {
             
             if (Input.GetMouseButtonDown(0))
             {
-                _onClick?.Invoke();　//　Actionを発火（Actionの中身がnullの場合を考慮）
+                if (_onClick == null)
+                {
+                    Debug.Log("クリック時の処理が一つも登録されていません");
+                }
+                else
+                {
+                    _onClick.Invoke();　
+                    //　Actionを発火（Actionの中身がnullの場合を考慮）
+                }
+              
             }
-            _image.color = Color.Lerp(_image.color, _OnCursorColor, Time.deltaTime);
+            
             
         }
        
     }
 
-    private bool OnMouseCursor()
+    private void OnMouseCursor()
     {
         RectTransform rectTransform = GetComponent<RectTransform>();
         Vector2 localPoint = Vector2.zero;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
             rectTransform, Input.mousePosition, Camera.main, out localPoint); // 第一引数に入れたRectTransformに対する第二引数のローカル座標をRectTransform基準で返す
-        return rectTransform.rect.Contains(localPoint);　//　カーソルのローカル座標が範囲内にあるかどうかを返す
+        OnCursor = rectTransform.rect.Contains(localPoint);　//　カーソルのローカル座標がimageの範囲内にあるかどうかを返す
+    }
+
+    IEnumerator ChangeColor(Color targetColor)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < _colorChangeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            _image.color = Color.Lerp(_image.color, targetColor, _colorChangeTime);
+            yield return null;
+        }
+        _colorCoroutine = null;
     }
 
   
