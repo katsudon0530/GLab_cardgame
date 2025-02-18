40d454c23a9e009a358d3509951482b39048c428
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-02-18 22:00:15 +0900

    [Add]コメントの追加　summaryではなくheaderに

--- Assets/Scripts/UI/ButtonUI.cs
+++ Assets/Scripts/UI/ButtonUI.cs
@@ -18,21 +18,22 @@ public class ButtonUI : MonoBehaviour
     public Action OnClick{get => _onClick; set => _onClick = value;}
     
     
-    /// <summary>
-    ///　カーソルがボタン上にあるときの色
-    /// </summary>
+    [Header("カーソルがボタン上にある時の色")]
     [SerializeField]
     private　Color _onCursorColor;
-    /// <summary>
-    ///　デフォルトのボタンの色
-    /// </summary>
+    
+    [Header("ボタンのデフォルトの色")]
     [SerializeField]
     private Color _defaultColor;
     
     private bool _onCursor;
     
+    /// <summary>
+    ///　遷移中のコルーチンがこの変数に都度保存される
+    /// </summary>
     private Coroutine _colorCoroutine;
     
+    [Header("色が切り替わるための遷移時間")]
     [SerializeField]
     private float _colorChangeTime = 0.5f;  
 
@@ -41,14 +42,16 @@ public class ButtonUI : MonoBehaviour
         get => _onCursor;
         set
         {
-            if (_onCursor != value)
+            if (_onCursor != value)　// カーソルがボタン上にあるか判定しているboolの値が切り替わったとき
             {
-                _onCursor = value;
-                if (_colorCoroutine != null)
+                _onCursor = value;　// 変更を適用
+                if (_colorCoroutine != null)　
                 {
                     StopCoroutine(_colorCoroutine);
+                    // 色の変更の遷移中であるときに_onCursorが切り替わった時に現在の遷移を止める　
                 }
                 _colorCoroutine = StartCoroutine(ChangeColor(_onCursor ? _onCursorColor : _defaultColor));
+                //色の切り替え開始　遷移中のコルーチンを変数に保存(遷移中であるかどうかの判定のため)
             }
         }
     }
@@ -56,7 +59,7 @@ public class ButtonUI : MonoBehaviour
     private void Awake()
     {
         _image = GetComponent<Image>();
-        _image.color = _defaultColor;
+        _image.color = _defaultColor;　
         _onClick += ForDebug;
     }
     void ForDebug()
@@ -69,8 +72,7 @@ public class ButtonUI : MonoBehaviour
         OnMouseCursor();
         if (OnCursor)
         {
-            
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0)) //左クリック時に
             {
                 if (_onClick == null)
                 {
@@ -99,16 +101,16 @@ public class ButtonUI : MonoBehaviour
 
     IEnumerator ChangeColor(Color targetColor)
     {
-        Color startColor = _image.color;
-        float elapsedTime = 0;
-        while (elapsedTime < _colorChangeTime)
+        Color startColor = _image.color;　//Colorの初期値を保存
+        float elapsedTime = 0;　// 現在の遷移時間
+        while (elapsedTime < _colorChangeTime)　// 決められた遷移時間になるまで繰り返す
         {
-            elapsedTime += Time.deltaTime;
-            _image.color = Color.Lerp(startColor, targetColor, elapsedTime /_colorChangeTime);
-            yield return null;
+            elapsedTime += Time.deltaTime;　// 遷移時間を計算していく 
+            _image.color = Color.Lerp(startColor, targetColor, elapsedTime /_colorChangeTime); //色を徐々にtargetColorに切り替える（Lerpの第三引数は0~1の間で遷移するように）
+            yield return null;　
         }
-        _image.color = targetColor;
-        _colorCoroutine = null;
+        _image.color = targetColor;　// 最終的な色の変更
+        _colorCoroutine = null;　// 遷移が終わったので中身を空にする
     }
 
   
