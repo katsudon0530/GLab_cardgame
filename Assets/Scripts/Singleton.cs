e02c44e01b0b4d40953f4b87fcf97260f90dcc87
tokumotokotetu <k824tokumoto@gmail.com>
2025-02-19 13:40:48 +0900

    [add]シングルトーンを追加

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    private static readonly object _lock = new object();
+    
+    public static T Instance
+    {
+        get
+        {
+            lock(_lock) //同時アクセスを防ぐ
+            {
+                if (_instance == null)
+                {
+                    _instance = (T)FindObjectOfType(T);
+                    
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject(typeof(T).Name);
+                        _instance = obj.AddComponent<T>();
+                        DontDestroyOnLoad(obj);
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnApplicationQuit()
+    {
+        _instance = null;
+    }
+}
