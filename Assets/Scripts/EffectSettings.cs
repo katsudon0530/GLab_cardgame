d987a06cee3371070fa74a16eb44c8829182a86d
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-03-04 21:43:32 +0900

    [Add]cts追加等

--- Assets/Scripts/EffectSettings.cs
+++ Assets/Scripts/EffectSettings.cs
@@ -1,21 +1,43 @@
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using System;
+using System.Threading;
 
 public class EffectSettings : MonoBehaviour
 {
     [SerializeField] private AnimationCurve _easing;
     [SerializeField] private float _duration;
-    public void EffectInstantiate(Vector3 effectPos)
+    [SerializeField] private float _lifeTime;
+    private Action _onDestroy;
+    private CancellationTokenSource _cts;
+    public async UniTask StayEffect(Vector3 effectPos)
     {
+        _cts = new CancellationTokenSource();
         GameObject effect = (GameObject)Resources.Load("Effect");
-        Instantiate(effect, effectPos, Quaternion.identity);
+        var effectPrefab =  Instantiate(effect, effectPos, Quaternion.identity);
+        await UniTask.Delay(TimeSpan.FromSeconds(_lifeTime), cancellationToken:_cts.Token);
+        _onDestroy?.Invoke();
+        Destroy(effectPrefab);
     }
 
-    public async UniTask EffectMove(Vector3 targetPos)
+    public async UniTask MoveEffect(Vector3 targetPos, Vector3 startPos)
     {
-         await transform.DOLocalMove(targetPos, _duration).SetEase(_easing).ToUniTask();
-         Debug.Log("移動完了");
+        GameObject effect = (GameObject)Resources.Load("Effect");
+        var effectPrefab =  Instantiate(effect, startPos, Quaternion.identity);
+        await effect.transform.DOLocalMove(targetPos, _duration).SetEase(_easing).ToUniTask(cancellationToken:_cts.Token);
+        _onDestroy?.Invoke();
+        Destroy(effectPrefab);
          
+    }
+
+    public void CancellEffect()
+    {
+        _cts?.Cancel();
+    }
+
+    public void Dispose()
+    {
+        _cts?.Dispose();
     }
 }
