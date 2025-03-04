a0137600a0caf15764a44fd0b55b75231f37c3b4
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-03-04 17:26:16 +0900

    [Add]unitask導入等

--- Assets/Scripts/EffectSettings.cs
+++ Assets/Scripts/EffectSettings.cs
@@ -1,16 +1,21 @@
-
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 
 public class EffectSettings : MonoBehaviour
 {
+    [SerializeField] private AnimationCurve _easing;
+    [SerializeField] private float _duration;
     public void EffectInstantiate(Vector3 effectPos)
     {
         GameObject effect = (GameObject)Resources.Load("Effect");
         Instantiate(effect, effectPos, Quaternion.identity);
     }
 
-    public void EffectMove(Vector3 targetPos)
+    public async UniTask EffectMove(Vector3 targetPos)
     {
-        
+         await transform.DOLocalMove(targetPos, _duration).SetEase(_easing).ToUniTask();
+         Debug.Log("移動完了");
+         
     }
 }
